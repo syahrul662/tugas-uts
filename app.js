58bed9c80c20d49bdc0cbbba55d2e930e175c470
syahrul662 <62542898+syahrul662@users.noreply.github.com>
2021-04-30 14:00:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ app.get("/bmi", function(req, res){
 
 app.post("/bmi", function(req, res){
 
-     var weight= Number(req.body.weight);
-    var height = Number(req.body.height);
-    var hasil = (weight/(height/100*2));
-             
+    weight= Number(req.body.weight);
+    height = Number(req.body.height);
+    hasil = (weight/(height/100*2));
+    hasil = hasil.toPrecision(3);         
                  
     
      if (hasil < 17.0){ 
@@ -25,7 +25,7 @@ app.post("/bmi", function(req, res){
     else if (hasil >= 17.0 && hasil < 24.0 ){ 
          res.send("<centre><h1>Body mask index =" + hasil + "<centre><h1>kamu ideal!");
      }
-    else if (hasil >= 24.0){ 
+    else if (hasil >= 24.0 && hasil < 27.0){ 
          res.send("<centre><h1>Body mask index =" + hasil + "<centre><h1>kamu Gemuk!");     
     }
     else (hasil > 27.0)   
